08e7e49ae37e6f1a963e7d095d92b9492ff244a8
TuoAdama <tuoadama17@gmail.com>
2023-10-07 22:18:45 +0200

    fix: search base on api configuration

--- public/js/search/search.js
+++ public/js/search/search.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     const parameters = {
-        url: "https://trouverunlogement.lescrous.fr/photon/api",
+        url: "https://trouverunlogement.lescrous.fr/photon/api?limit=18&osm_tag=amenity%3Acollege&osm_tag=amenity%3Alibrary&osm_tag=amenity%3Aschool&osm_tag=amenity%3Auniversity&osm_tag=place%3Acountry&osm_tag=place%3Aregion&osm_tag=place%3Astate&osm_tag=place%3Acity&osm_tag=place%3Atown&osm_tag=place%3Avillage&osm_tag=place%3Ahouse&osm_tag=landuse%3Aresidential",
         country: "France",
         limit: 18,
         lang: "fr",
@@ -11,16 +11,13 @@ $(document).ready(function() {
     $('select').select2({
         minimumInputLength: parameters.minimumInputLength,
         width: "100%",
-        placeholder: 'Exemple: Rennes',
+        placeholder: "Exemple: Rennes, Résidence ou lieu d'étude",
         dropdownParent: $('#dropdown'),
         ajax: {
             url: parameters.url,
             data: function(params){
                 return {
-                    q: params.term,
-                    limit: 18,
-                    lang: parameters.lang,
-                    osm_tag: "place:city"
+                    q: params.term
                 }
             },
             processResults: function (data) {
