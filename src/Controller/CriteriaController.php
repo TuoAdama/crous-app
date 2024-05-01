a10810889b855e338bc91addd91c2aa882f127e8
Tuo Adama <tuoadama17@gmail.com>
2024-05-01 18:59:25 +0200

    feat: partial => flash message

--- src/Controller/CriteriaController.php
+++ src/Controller/CriteriaController.php
@@ -3,15 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\SearchCriteria;
+use App\Form\NotificationType;
 use App\Form\SearchCriteriaType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class CriteriaController extends AbstractController
 {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly TranslatorInterface $translator,
+    )
+    {
+    }
+
     #[Route('/criteria', name: 'app_criteria')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function index(Request $request): Response
@@ -19,12 +29,19 @@ class CriteriaController extends AbstractController
         $searchCriteria = new SearchCriteria();
         $form = $this->createForm(SearchCriteriaType::class, $searchCriteria);
         $form->handleRequest($request);
-        if ($form->isSubmitted()){
-            dd($form);
+        if ($form->isSubmitted() && $form->isValid()){
+            dd($form->getData());
+        }
+        $notificationForm = $this->createForm(NotificationType::class, $this->getUser());
+        $notificationForm->handleRequest($request);
+        if ($notificationForm->isSubmitted() && $notificationForm->isValid()){
+            $this->entityManager->flush();
+            $this->addFlash('success', $this->translator->trans('flash.messages.update'));
         }
         return $this->render('pages/criteria/criteria.html.twig',  [
             'identifier' => $this->getUser()->getUserIdentifier(),
             'form' => $form,
+            'notificationForm' => $notificationForm,
         ]);
     }
 }
