b37a01419d48f9ce535f5c914b8d79f5e5448113
Tuo Adama <tuoadama17@gmail.com>
2024-05-27 14:42:16 +0200

    feat: user contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            dd("OK");
+        }
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form,
         ]);
     }
 }
