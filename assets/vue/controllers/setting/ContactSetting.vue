8d52236d51bf42db7a2fbeac3d7a3b54215043cb
TuoAdama <tuoadama17@gmail.com>
2024-11-09 22:04:58 +0100

    feat: edit email form

--- assets/vue/controllers/setting/ContactSetting.vue
+++ assets/vue/controllers/setting/ContactSetting.vue
@@ -1,26 +1,42 @@
 <script setup>
 import User from "../models/User";
+import {ref} from "vue";
+import EditEmailForm from "./EditEmailForm.vue";
 
 const props = defineProps({
   user: User
 })
+
+const edit = ref(false);
 
 </script>
 
 <template>
   <div class="card h-100">
     <div class="card-body">
-      <h6 class="d-flex align-items-center mb-3">Coordonnées</h6>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-          <h6 class="mb-0">Email:</h6>
-          <span class="text-secondary">{{user.email}}</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-          <h6 class="mb-0">Téléphone:</h6>
-          <span class="text-secondary">{{user.number ?? "Aucun numéro"}}</span>
-        </li>
-      </ul>
+      <template v-if="!edit">
+        <h6 class="d-flex align-items-center mb-3">Coordonnées</h6>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+            <h6 class="mb-0">Email:</h6>
+            <span class="text-secondary">
+            {{user.email}}
+
+            <button class="ms-3 btn btn-secondary" @click="edit = true">Modifier</button>
+          </span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+            <h6 class="mb-0">Téléphone:</h6>
+            <span class="text-secondary">
+            {{user.number ?? "Aucun numéro"}}
+            <button class="ms-3 btn btn-secondary">
+              {{user.number ? 'Modifier': 'Ajouter'}}
+            </button>
+          </span>
+          </li>
+        </ul>
+      </template>
+      <EditEmailForm v-if="edit" :onCancel="() => edit = false"/>
     </div>
   </div>
 </template>
---
+++ assets/vue/controllers/setting/EditEmailForm.vue
@@ -0,0 +1,44 @@
+<script setup>
+  const props = defineProps(["onCancel"])
+</script>
+
+<template>
+  <div class="p-3">
+    <div class="row mb-3">
+      <div class="col-sm-6">
+        <h6 class="mb-0">Nouvelle adresse e-mail:</h6>
+      </div>
+      <div class="col-sm-6">
+        <input class="form-control text-secondary m-0" type="text">
+      </div>
+    </div>
+    <hr>
+    <div class="row mb-3">
+      <div class="col-sm-6">
+        <h6 class="mb-0">Confirmation de l'e-mail:</h6>
+      </div>
+      <div class="col-sm-6">
+        <input class="form-control text-secondary m-0" type="text">
+      </div>
+    </div>
+    <hr>
+    <div class="row mb-3">
+      <div class="col-sm-6">
+        <h6 class="mb-0">Mot de passe:</h6>
+      </div>
+      <div class="col-sm-6">
+        <input class="form-control text-secondary m-0" type="text">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <button class="btn btn-primary">Enregistrer</button>
+        <button class="btn btn-danger ms-3" @click="props.onCancel">Annuler</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
