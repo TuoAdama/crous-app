f77bd26382a7ffaef757babc0f108bee3dcbd796
TuoAdama <tuoadama17@gmail.com>
2024-11-10 14:16:38 +0100

    feat: email and password edit form

---
+++ assets/vue/controllers/setting/AddNumberSetting.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import {PropType} from "vue";
+
+  const props = defineProps({
+    onCancel: {
+      type: Function as PropType<() => void>,
+      required: true
+    }
+  })
+</script>
+
+<template>
+  <div class="p-3">
+    <div class="row mb-3">
+      <div class="col-sm-12">
+        <h6 class="mb-0">Numéro de téléphone:</h6>
+      </div>
+      <div class="col-12 col-lg-8 mt-3">
+        <input class="form-control text-secondary m-0" type="text">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <button class="btn btn-primary">Enregistrer</button>
+        <button class="btn btn-danger ms-3" @click="props.onCancel">Annuler</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- assets/vue/controllers/setting/ContactSetting.vue
+++ assets/vue/controllers/setting/ContactSetting.vue
@@ -2,19 +2,27 @@
 import User from "../models/User";
 import {ref} from "vue";
 import EditEmailForm from "./EditEmailForm.vue";
+import AddNumberSetting from "./AddNumberSetting.vue";
 
 const props = defineProps({
   user: User
 })
 
 const edit = ref(false);
+const isAddNumber = ref(false);
+const isEditEmail = ref(false);
+
+const onAddNumber = () => {
+  isAddNumber.value = true;
+  edit.value = true;
+}
 
 </script>
 
 <template>
   <div class="card h-100">
     <div class="card-body">
-      <template v-if="!edit">
+      <template v-if="!isEditEmail && !isAddNumber">
         <h6 class="d-flex align-items-center mb-3">Coordonnées</h6>
         <ul class="list-group list-group-flush">
           <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
@@ -22,21 +30,21 @@ const edit = ref(false);
             <span class="text-secondary">
             {{user.email}}
 
-            <button class="ms-3 btn btn-secondary" @click="edit = true">Modifier</button>
+            <button class="ms-3 btn btn-secondary" @click="isEditEmail = true">Modifier</button>
           </span>
           </li>
           <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
             <h6 class="mb-0">Téléphone:</h6>
             <span class="text-secondary">
             {{user.number ?? "Aucun numéro"}}
-            <button class="ms-3 btn btn-secondary">
-              {{user.number ? 'Modifier': 'Ajouter'}}
-            </button>
+            <button v-if="user.number" class="ms-3 btn btn-secondary" >Modifier</button>
+            <button v-if="!user.number" class="ms-3 btn btn-secondary" @click="onAddNumber">Ajouter</button>
           </span>
           </li>
         </ul>
       </template>
-      <EditEmailForm v-if="edit" :onCancel="() => edit = false"/>
+      <EditEmailForm v-if="isEditEmail && !isAddNumber" :on-cancel="() => isEditEmail = false"/>
+      <AddNumberSetting v-if="isAddNumber && !isEditEmail" :on-cancel="() => isAddNumber = false"/>
     </div>
   </div>
 </template>
