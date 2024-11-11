ebd8f37b2b4c0523f28299672dfb4b4fa6c62211
TuoAdama <tuoadama17@gmail.com>
2024-11-11 23:28:27 +0100

    feat: refactor

--- assets/vue/controllers/setting/SettingComponent.vue
+++ assets/vue/controllers/setting/SettingComponent.vue
@@ -25,9 +25,8 @@
     <div class="main-body">
       <nav aria-label="breadcrumb" class="main-breadcrumb">
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
-          <li class="breadcrumb-item active" aria-current="page">User Profile</li>
+          <li class="breadcrumb-item"><a href="/">Accueil</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Compte</li>
         </ol>
       </nav>
 
