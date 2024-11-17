357860b597dafb368a33f6dfe08ea0f152ca2084
TuoAdama <tuoadama17@gmail.com>
2024-11-17 18:22:55 +0100

    feat: scheduler search result parameters

--- src/Scheduler/FindCriteriaResultSchedulerProvider.php
+++ src/Scheduler/FindCriteriaResultSchedulerProvider.php
@@ -3,6 +3,7 @@
 namespace App\Scheduler;
 
 use App\Message\FindCriteriaResultMessage;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
 use Symfony\Component\Scheduler\RecurringMessage;
 use Symfony\Component\Scheduler\Schedule;
@@ -11,10 +12,17 @@ use Symfony\Component\Scheduler\ScheduleProviderInterface;
 #[AsSchedule('find_criteria_result')]
 class FindCriteriaResultSchedulerProvider implements ScheduleProviderInterface
 {
+    private int $minutes;
+    public function __construct(
+        ParameterBagInterface $parameterBag,
+    )
+    {
+        $this->minutes = $parameterBag->get('criteria.search.result.schedule');
+    }
 
     public function getSchedule(): Schedule
     {
         return $this->schedule ??= (new Schedule())
-        ->add(RecurringMessage::cron("*/1 * * * *", new FindCriteriaResultMessage()));
+        ->add(RecurringMessage::every($this->minutes." minutes", new FindCriteriaResultMessage()));
     }
 }
