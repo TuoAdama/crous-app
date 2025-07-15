55d3fb66635a646e69aa890b152c93aa9bcf4bda
TuoAdama <tuoadama17@gmail.com>
2025-07-15 20:22:56 +0200

    feat: remove unused navbar item

--- assets/vue/controllers/home/Navbar.vue
+++ assets/vue/controllers/home/Navbar.vue
@@ -27,12 +27,10 @@ function toggleMenu() {
       </div>
       <div :class="['md:flex md:space-x-4 mt-4 md:mt-0', { 'hidden': !isMenuOpen, 'block': isMenuOpen }]">
         <a href="#" class="block md:inline text-gray-600 hover:text-blue-600 py-2">Accueil</a>
-        <a href="#" class="block md:inline text-gray-600 hover:text-blue-600 py-2">Rechercher</a>
-        <a v-if="isAuth" href="#" class="block md:inline text-gray-600 hover:text-blue-600 py-2">Favoris</a>
         <a v-if="isAuth" href="/profile" class="block md:inline text-gray-600 hover:text-blue-600 py-2">Profil</a>
-        <button v-if="!isAuth" class="block md:inline bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 mt-2 md:mt-0">
+        <a v-if="!isAuth" href="/login" class="block md:inline bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 mt-2 md:mt-0">
           Connexion
-        </button>
+        </a>
       </div>
     </div>
   </nav>
